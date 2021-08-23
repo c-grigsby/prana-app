b9f22fee9feb051724c7c25ba690fc70e145a906
grigsby9 <grigsby.c@outlook.com>
2021-08-23 01:14:29 -0400

    feat(data): integrated create and read

--- src/store/actions/locations-actions.js
+++ src/store/actions/locations-actions.js
@@ -1,7 +1,9 @@
 // @packages
 import * as FileSystem from 'expo-file-system';
-
+// @scripts
 export const ADD_LOCATION = 'ADD_LOCATION';
+export const SET_LOCATIONS = 'SET_LOCATIONS';
+import { fetchLocations, insertLocation } from '../../helpers/db';
 
 export const addLocation = (title, image) => {
   return async (dispatch) => {
@@ -9,18 +11,37 @@ export const addLocation = (title, image) => {
     const newPath = FileSystem.documentDirectory + fileName;
 
     try {
-      FileSystem.moveAsync({
+      await FileSystem.moveAsync({
         from: image,
         to: newPath,
+      });
+      const dbResult = await insertLocation(
+        title,
+        newPath,
+        'Fake address',
+        16.6,
+        14.3
+      );
+      console.log(dbResult);
+      dispatch({
+        type: ADD_LOCATION,
+        locationData: { id: dbResult.insertId, title: title, image: newPath },
       });
     } catch (err) {
       console.log(err);
       throw err;
     }
+  };
+};
 
-    dispatch({
-      type: ADD_LOCATION,
-      locationData: { title: title, image: newPath },
-    });
+export const loadLocations = () => {
+  return async (dispatch) => {
+    try {
+      const dbResult = await fetchLocations();
+      console.log(dbResult);
+      dispatch({ type: SET_LOCATIONS, locations: dbResult.rows._array });
+    } catch (err) {
+      throw err;
+    }
   };
 };
